716a9b1806dc95c8935f32dbf54b102a1052d945
Peter Jones <pjxcvi@proton.me>
2023-05-15 22:17:14 -0400

    add project files

---
+++ public/src/accounts.js
@@ -0,0 +1,34 @@
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
+
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((nameA, nameB) => nameA.name.last > nameB.name.last ? 1 : -1);
+}
+
+function getTotalNumberOfBorrows(account, books) {
+  const userID = account.id;
+  let counter = 0;
+  const total = books.reduce((account, books) => {
+    const borrowRecord = books.borrows;
+    const mapID = borrowRecord.map((record) => record.id);
+    if(mapID.includes(userID)) account++;
+    return account;
+  }, counter);
+  return total;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books.filter((book) => book.borrows.some(acc => acc.id === account.id && acc.returned === false))
+    .map(book => { const author = authors.find(author => author.id === books.authorId)
+      book.author = author;
+      return book;
+    })
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
